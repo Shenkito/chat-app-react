de9ab4c523032c8d141663b07551a668b30572fb
Ivelin Shenkov <ishenkito@gmail.com>
2024-03-02 15:32:41 +0200

    Add server , mongoose and cors

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const { Message } = require('./models/Message');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/chat-app-react`)
+    .then(() => console.log('DB connected...'))
+    .catch(err => console.log('DB error', err.message));
+app.use(express.json());
+
+app.post('/messages', async (req, res) => {
+    try {
+        const { text, sender } = req.body;
+        const message = new Message({ text, sender });
+        await message.save();
+        res.status(201).json(message);
+    } catch (error) {
+        res.status(400).json({ error: error.message});
+    }
+});
+
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001...');
+});
