424e9f9d6d087677078bcffce60d1196fef8f2b5
Ivelin Shenkov <ishenkito@gmail.com>
2024-03-22 15:55:42 +0200

    Fixed to Link

--- client/src/components/SignupLoginSection/SignupLoginSection.jsx
+++ client/src/components/SignupLoginSection/SignupLoginSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../SignupLoginSection/SignupLoginSection.css';
 
 export default function SignupLoginSection() {
@@ -6,8 +7,8 @@ export default function SignupLoginSection() {
             <h2>Get Started with Whisper</h2>
             <p>Join our community today and start connecting with friends!</p>
             <div className="cta-buttons">
-                <a href="/signup" className="btn btn-primary">Sign Up</a>
-                <a href="/login" className="btn btn-secondary">Log In</a>
+                <Link to="/login" className="btn btn-primary">Log In</Link>
+                <Link to="/signup" className="btn btn-secondary">Sign Up</Link>
             </div>
         </section>
     )
