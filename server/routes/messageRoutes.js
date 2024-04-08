be5bd057c6354a26d9a4713b74f7d139a74291d6
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-08 21:22:12 +0300

    Add get route

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { sendMessage } from '../controllers/messageController.js';
+import { sendMessage, getMessages } from '../controllers/messageController.js';
 import protectRoute from '../middleware/protectRoute.js'
 
 const router = express.Router();
 
+
+router.get("/:id", protectRoute, getMessages);
 //                AUTHORIZATION (middleware)
 router.post("/send/:id", protectRoute, sendMessage);
 
