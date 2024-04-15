5a1c9fa9e3cbcf0e2ff1d81be7323ed777c65400
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-15 23:20:28 +0300

    Add tailwindcss and daisyUi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 
-import "../src/App.css"
-
-// import MessageForm from './components/MessageForm/MessageForm';
-import NavBar from './components/NavBar/NavBar';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import SignUp from './pages/Signup/Signup';
 
 function App() {
 
@@ -14,9 +7,7 @@ function App() {
         <div className="main-app">
             <NavBar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
+                
             </Routes>
         </div>
     )
