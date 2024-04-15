adbd0f2a70492ffc9d4fbc88fa5d6c77b4f90dbc
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-16 00:01:55 +0300

    Add index css with tailwind and daisyUi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
 
     return (
-        <div className="main-app">
-            <NavBar />
-            <Routes>
-                
-            </Routes>
-        </div>
+        <>
+            <button className="btn">Button</button>
+            <button className="btn btn-neutral">Neutral</button>
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-secondary">Secondary</button>
+            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-link">Link</button>
+        </>
     )
 }
 
