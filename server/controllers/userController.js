715223c926aa697c0d506e86340febb66995be4b
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-15 23:03:11 +0300

    Add user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+import User from "../models/userModel.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user_id;
+
+        // Prevent to have the current authenticated user profile in the user lists $ne
+        const allUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+
+        res.status(200).json(allUsers);
+    } catch (error) {
+        console.log("Error in getUsersForSidebar:", error.message)
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
