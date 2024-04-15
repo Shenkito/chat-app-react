cc0288a6d566a79d42a48f705e55a0ad2bd9b613
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-15 23:03:53 +0300

    Add user routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/authRoutes.js'
 import messageRoutes from './routes/messageRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -19,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(cors());
 
