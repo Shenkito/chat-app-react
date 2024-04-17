cb85f14d30c7fbee93a0e8396b3a916d33ed0394
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-17 21:26:22 +0300

    Add login page

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+const Login = () => {
+    return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+        <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            <h1 className="text-3xl font-semibold text-center text-black">
+                Login to
+                <span className="text-blue-500"> Whisper </span>
+            </h1>
+
+            <form>
+                <div>
+                    <label className="label p-2">
+                        <span className="text-base label-text text-black">Username</span>
+                    </label>
+                    <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10" />
+                </div>
+            </form>
+        </div>
+    </div>
+}
+
+export default Login
