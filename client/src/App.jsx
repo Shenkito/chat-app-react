26596cc98ce4e00f654fd116806655ef3bc0aaba
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-17 21:07:09 +0300

    Add login page to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Login from './pages/Login/Login';
 
 function App() {
 
     return (
-        <>
-            <button className="btn">Button</button>
-            <button className="btn btn-neutral">Neutral</button>
-            <button className="btn btn-primary">Primary</button>
-            <button className="btn btn-secondary">Secondary</button>
-            <button className="btn btn-accent">Accent</button>
-            <button className="btn btn-ghost">Ghost</button>
-            <button className="btn btn-link">Link</button>
-        </>
+        <div className='p-4 h-screen flex items-center justify-center'>
+            <Login />
+        </div>
     )
 }
 
