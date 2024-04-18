985833b15bda68689509e9bfc8438adf2955c7a4
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-18 21:05:53 +0300

    Update login with password

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 const Login = () => {
     return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-        <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <div className="w-full p-6 rounded-xl shadow-xl bg-cyan-50 bg-clip-padding">
             <h1 className="text-3xl font-semibold text-center text-black">
                 Login to
                 <span className="text-blue-500"> Whisper </span>
@@ -12,6 +12,12 @@ const Login = () => {
                         <span className="text-base label-text text-black">Username</span>
                     </label>
                     <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10" />
+                </div>
+                <div>
+                    <label className="label p-2">
+                        <span className="text-base label-text text-black">Password</span>
+                    </label>
+                    <input type="text" placeholder="Enter password" className="w-full input input-bordered h-10" />
                 </div>
             </form>
         </div>
