b793f422579e5da817fbd745a1519539089aa988
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-18 21:41:06 +0300

    Add button and dont have account link

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -17,11 +17,19 @@ const Login = () => {
                     <label className="label p-2">
                         <span className="text-base label-text text-black">Password</span>
                     </label>
-                    <input type="text" placeholder="Enter password" className="w-full input input-bordered h-10" />
+                    <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10" />
                 </div>
+                <div className="text-center">
+                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                        {" Don't "} have an account ?
+                    </a>
+                </div>
+                <button className="btn btn-block btn-sm mt-2 hover:text-blue-600"> Login </button>
             </form>
         </div>
     </div>
+
+
 }
 
 export default Login
