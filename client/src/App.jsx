880377fe38a2af67b6445ebf9189eab8d2799e50
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-25 21:15:58 +0300

    Add sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
 
     return (
         <div className='p-4 h-screen flex items-center justify-center'>
             <Login />
+            <SignUp />
         </div>
     )
 }
