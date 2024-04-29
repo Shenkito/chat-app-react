2c115a5a9fc6c608a4b29daa1af33847e63d8ffd
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-29 21:41:27 +0300

    Add home to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import Home from './pages/Home/Home';
 
 function App() {
 
     return (
         <div className='p-4 h-screen flex items-center justify-center'>
-            <Login />
-            <SignUp />
+            {/* <Login />
+            <SignUp /> */}
+            <Home />
         </div>
     )
 }
