9f0f5cd4780c5373cb5cc95523df05140dff8417
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-29 21:40:10 +0300

    Add home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import Sidebar from "../../components/Sidebar/Sidebar"
+
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <div className="flex sm:h-[450px] md:h-[550px] rounded-xl shadow-xl bg-cyan-50 bg-clip-padding">
+            
+            <Sidebar />
+            {/* <MessageContainer /> */}
+
+        </div>
+    )
 }
 
 export default Home
