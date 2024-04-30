15a990eeb45be43de6fb4e29eb23fec0b8d93678
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-30 21:46:18 +0300

    Add message container

---
+++ client/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,21 @@
+import Messages from "./Messages"
+
+const MessageContainer = () => {
+    return (
+
+        <div className="md:min-w-[450px] flex flex-col">
+            <>
+            <div className="bg-sky-500 px-4 py-2 mb-2">
+                <span className="label-text text-white">To: </span>
+                <span className="text-cyan-50 font-bold">JohnDoe</span>
+            </div>
+
+            <Messages />
+            {/* <MessageInput /> */}
+            </>
+        </div>
+
+    )
+}
+
+export default MessageContainer
