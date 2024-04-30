e385405d7d0c84f0fb11070dc757d765041b9071
Ivelin Shenkov <ishenkito@gmail.com>
2024-04-30 20:42:20 +0300

    Add logout button to sidebar

--- client/src/components/Sidebar/LogoutButton.jsx
+++ client/src/components/Sidebar/LogoutButton.jsx
@@ -1,7 +1,11 @@
+import { BiLogOut } from "react-icons/bi";
+
 const LogoutButton = () => {
     return (
 
-        <div className="mt-auto">Logout Button</div>
+        <div className="mt-auto">
+            <BiLogOut className="w-6 h-6 border-slate-500 cursor-pointer hover:text-blue-600" />
+        </div>
 
     )
 }
