60a6174829a5f5b352982a9ce629bf022e959ff6
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-06 21:13:40 +0300

    Fix the user avatar to appear the same as in the message component

--- client/src/components/Sidebar/Conversation.jsx
+++ client/src/components/Sidebar/Conversation.jsx
@@ -5,7 +5,7 @@ const Conversation = () => {
         <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
             <div className="avatar online">
                 <div className="w-12 rounded-full">
-                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt=" user avatar" />
+                <img alt="Tailwind CSS chat bubble component" src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" />
                 </div>
             </div>
             <div className="flex flex-col flex-1">
