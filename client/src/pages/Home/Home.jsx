db092437495f688b611af9c3fe9b5d70f81aa6a5
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-06 21:05:32 +0300

    Add overflow hidden and fix the rounded from xl to lg

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
     return (
 
-        <div className="flex sm:h-[450px] md:h-[550px] rounded-xl shadow-xl bg-cyan-50 bg-clip-padding">
+        <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden shadow-xl bg-cyan-50 bg-clip-padding">
             
             <Sidebar />
             <MessageContainer />
