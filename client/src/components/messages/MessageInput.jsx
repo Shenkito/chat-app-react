0b4201a135e28918ade63210ca8eb0d9c393c7bc
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-07 21:14:18 +0300

    Add hover to send button

--- client/src/components/messages/MessageInput.jsx
+++ client/src/components/messages/MessageInput.jsx
@@ -4,12 +4,12 @@ const MessageInput = () => {
     return (
 
         <form className="px-4 my-3">
-            <div className="w-full">
+            <div className="w-full relative">
                 <input type="text" 
                     className="border text-sm rounded-lg block w-full p-2.5 bg-gray-900 border-gray-600 text-white"
                     placeholder="Send a message"
                 />
-                <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3">
+                <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3 hover:text-blue-600">
                     <BsSend />
                 </button>
             </div>
