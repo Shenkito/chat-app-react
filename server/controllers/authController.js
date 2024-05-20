7a469dc6784920f3e96ac2837096cfdd00caa70e
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-20 21:57:21 +0300

    Fix error message

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,7 +8,7 @@ export const signUp = async (req, res) => {
         const { fullName, username, password, confirmPassword, gender, profilePicture } = req.body;
 
         if (password !== confirmPassword) {
-            return res.status(400).json({ error: "Passwords should be the same" })
+            return res.status(400).json({ error: "Passwords don't match" })
         }
 
         const user = await User.findOne({ username });
