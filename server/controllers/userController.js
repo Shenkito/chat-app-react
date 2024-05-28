5c6f2d323b9c79421dad6ab42870809ac67c0cd8
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-28 23:05:13 +0300

    Fix bug to not show current user in user list conversation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/userModel.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
-        const loggedInUserId = req.user_id;
+        const loggedInUserId = req.user._id;
 
         // Prevent to have the current authenticated user profile in the user lists $ne
         const allUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
