c022d4724919cfdd87a81979cb088910b7f7fe9b
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-28 23:52:36 +0300

    Add select chat functionality
    Add cleanup function to unmount the component when we logout.
    Add username to welcome message

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
@@ -5,18 +9,24 @@ import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
 
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    const { authUser } = useAuthContext();
+
+    //cleanup function (unmounts component)!!!
+    useEffect(() => {
+        return () => setSelectedConversation(null)
+    },[setSelectedConversation]);
 
     return (
 
         <div className="md:min-w-[450px] flex flex-col">
-            {noChatSelected ? (
-                <NoChatSelected />
+            {!selectedConversation ? (
+                <NoChatSelected authUser={authUser} />
             ) : (
                 <>
                     <div className="bg-sky-500 px-4 py-2 mb-2">
                         <span className="label-text text-white">To: </span>
-                        <span className="text-cyan-50 font-bold">JohnDoe</span>
+                        <span className="text-cyan-50 font-bold">{selectedConversation.fullName}</span>
                     </div>
 
                     <Messages />
@@ -30,13 +40,13 @@ const MessageContainer = () => {
 
 export default MessageContainer;
 
-const NoChatSelected = () => {
+const NoChatSelected = ({authUser}) => {
 
     return (
 
         <div className="flex items-center justify-center w-full h-full">
             <div className="px-4 text-center sm:text-lg md:text-xl text-gray-400 font-semibold flex flex-col items-center gap-2">
-                <p>Welcome JohnDoe</p>
+                <p>Welcome {authUser.username}</p>
                 <p>Select a chat to start messagin</p>
                 <TiMessages className="text-3xl md:text-6xl text-center" />
             </div>
