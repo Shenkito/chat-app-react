15a9a7ff57c3a5a31096f8395061d2775058c585
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-28 23:06:44 +0300

    Add real user data to user list conversation/s

--- client/src/components/Sidebar/Conversation.jsx
+++ client/src/components/Sidebar/Conversation.jsx
@@ -1,21 +1,25 @@
-const Conversation = () => {
+const Conversation = ({conversation, lastIdx, emoji}) => {
     
     return <>
 
         <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
             <div className="avatar online">
                 <div className="w-12 rounded-full">
-                <img alt="Tailwind CSS chat bubble component" src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" />
+                <img 
+                    alt="user avatar" 
+                    src={conversation.profilePicture}
+                    />
                 </div>
             </div>
             <div className="flex flex-col flex-1">
                 <div className="flex gap-3 justify-between">
-                    <p className="font-bold text-gray-400">JohnDoe</p>
-                    <span className="text-xl">emoji</span>
+                    <p className="font-bold text-gray-400">{conversation.fullName}</p>
+                    <span className="text-xl">{emoji}</span>
                 </div>
             </div>
         </div>
-        <div className="divider my-0 py-0 h-1" />
+        
+        {!lastIdx && <div className="divider my-0 py-0 h-1" />}
 
     </>
 
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -12,8 +12,6 @@ export const AuthContextProvider = ({ children }) => {
 
     const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
 
-    return <AuthContext.Provider value={{ authUser, setAuthUser }}>
-        {children}
-    </AuthContext.Provider>;
+    return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
 
 };
