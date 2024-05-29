919a7b470497902a02d1d0021746356b8e346784
Ivelin Shenkov <ishenkito@gmail.com>
2024-05-29 21:56:45 +0300

    Fix message component with real data

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,14 +1,30 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({message}) => {
+    const {authUser} = useAuthContext();
+    const {selectedConversation} = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe ? authUser.profilePicture : selectedConversation?.profilePicture;
+    const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+    const formattedTime = extractTime(message.createdAt);
+
+
     return (
 
-        <div className="chat chat-end">
+        <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-r-full">
-                    <img alt="Tailwind CSS chat bubble component" src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" />
+                    <img 
+                    alt="Tailwind CSS chat bubble component" 
+                    src={profilePic}
+                    />
                 </div>
             </div>
-            <div className={`chat-bubble text-white bg-blue-500`}>Hi! What's up ? :)</div>
-            <div className="chat-footer text-xs flex gap-1 items-center text-blue-500">8:40</div>
+            <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+            <div className="chat-footer text-xs flex gap-1 items-center text-blue-500">{formattedTime}</div>
         </div>
 
     );
