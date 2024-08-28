841594fc06cc08d5c52ef5bd29c9c681ed3c2206
Ivelin Shenkov <ishenkito@gmail.com>
2024-08-28 22:19:16 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
 
-import Home from './pages/home/Home';
+import Home from '../src/pages/Home/Home';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 
