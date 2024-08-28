494b42a41b8be59360ece9dddb68450e47ceea1c
Ivelin Shenkov <ishenkito@gmail.com>
2024-08-28 22:42:58 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { useAuthContext } from './context/AuthContext';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/Signup/SignUp';
 
 function App() {
 
--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -11,7 +11,7 @@ const generateTokenAndSetCookie = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // The lifetime of the cookie
         httpOnly: true, // Prevent XSS attacks (cross-site scripting attacks)
         sameSite: "strict", // CSRF attacks (cross-site request forgery attacks)
-        // secure: process.env.NODE_ENV !== "development"
+        secure: process.env.NODE_ENV !== "development"
     });
 };
 
