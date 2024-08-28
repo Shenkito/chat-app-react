3eb8c15dce5617a349387cc1dc4effbd88e7a29b
Ivelin Shenkov <ishenkito@gmail.com>
2024-08-28 22:26:25 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
 
-import Home from '../src/pages/Home/Home';
-import Login from '../src/pages/Login/Login';
-import SignUp from '../src/pages/SignUp/SignUp';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
 
 function App() {
 
