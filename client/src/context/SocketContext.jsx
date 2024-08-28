036f4b5bbf4a3020c3da8ee760dca723c3936b79
Ivelin Shenkov <ishenkito@gmail.com>
2024-08-28 23:19:48 +0300

    Update socket context with url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:3001", {
+            const socket = io("https://chat-app-react-are6.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
